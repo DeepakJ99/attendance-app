8f41bc05f18321d108483cb7e5e2a3b6390f80ae
DeepakJ99 <36583641+DeepakJ99@users.noreply.github.com>
2023-02-16 00:23:14 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/attendance',{
+mongoose.connect('mongodb+srv://djakhu99:H2S1VuzP1KN4dB96@cluster0.istmr66.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useCreateIndex:true
 })
